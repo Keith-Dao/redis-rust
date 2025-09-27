3dc63caeaecbaae999fd61e9b4c5d342ea2d9428
Keith Dao <keith.dao.248@gmail.com>
2025-09-27 21:06:47 +1000

    Update to respond with an error if SET cannot be completed.

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -5,31 +5,27 @@ use anyhow::{Context, Result};
 /// Parses the SET options and returns the entry if successful.
 fn parse_set_options<I: IntoIterator<Item = resp::RespType>>(iter: I) -> Result<store::Entry> {
     let mut iter = iter.into_iter();
-    let value = resp::extract_string(
-        &iter
-            .next()
-            .ok_or(anyhow::anyhow!("Missing value option."))?,
-    )
-    .context("Failed to extract value.")?;
-    let mut entry = store::Entry::new(value);
+    let value = resp::extract_string(&iter.next().ok_or(anyhow::anyhow!("Missing value"))?)
+        .context("Failed to extract value")?;
 
+    let mut entry = store::Entry::new(value);
     while let Some(token) = &iter.next() {
-        let option = resp::extract_string(token).context("Failed to extract option.")?;
+        let option = resp::extract_string(token).context("Failed to extract option")?;
 
         match option.to_lowercase().as_str() {
             "px" => {
                 let duration = resp::extract_string(
                     &iter
                         .next()
-                        .ok_or(anyhow::anyhow!("Missing milliseconds for PX option."))?,
+                        .ok_or(anyhow::anyhow!("Missing milliseconds for PX option"))?,
                 )
-                .context("Failed to extract duration string.")?
+                .context("Failed to extract duration string")?
                 .parse::<u64>()
-                .context("Failed to convert duration string to a number.")?;
+                .context("Failed to convert PX duration string to a number")?;
                 entry = entry.with_deletion(duration);
             }
             _ => {
-                return Err(anyhow::anyhow!("An invalid option was provided: {option}."));
+                return Err(anyhow::anyhow!("{option} is not a valid option"));
             }
         }
     }
@@ -38,32 +34,27 @@ fn parse_set_options<I: IntoIterator<Item = resp::RespType>>(iter: I) -> Result<
 }
 /// Handles the SET command.
 pub async fn handle(args: Vec<resp::RespType>, store: &store::Store) -> resp::RespType {
-    let failure_result = resp::RespType::Null();
     let mut args = args.into_iter();
 
-    let key;
-    if let Some(key_token) = &args.next() {
-        if let Ok(result) = resp::extract_string(key_token) {
-            key = result;
-        } else {
-            log::error!("Failed to extract key string from: {:?}", key_token);
-            return failure_result;
+    let key = match args
+        .next()
+        .context("Missing key")
+        .and_then(|key_token| resp::extract_string(&key_token).context("Failed to extract key"))
+    {
+        Ok(result) => result,
+        Err(err) => {
+            log::error!("Failed to extract the key. Err: {err}");
+            return resp::RespType::BulkError(format!("ERR {err} for 'SET' command"));
         }
-    } else {
-        log::error!("Key was not provided.");
-        return failure_result;
-    }
+    };
 
-    let entry;
-    match parse_set_options(args) {
-        Ok(result) => {
-            entry = result;
-        }
+    let entry = match parse_set_options(args) {
+        Ok(result) => result,
         Err(err) => {
             log::error!("{err}");
-            return failure_result;
+            return resp::RespType::BulkError(format!("ERR {err} for 'SET' command"));
         }
-    }
+    };
 
     store.lock().await.insert(key, entry);
     resp::RespType::SimpleString("OK".into())
@@ -106,14 +97,21 @@ mod tests {
     }
 
     #[rstest]
+    #[case::px_upper("PX")]
+    #[case::px_lower("px")]
     #[tokio::test]
-    async fn test_handle_with_px(store: crate::store::Store, key: String, value: String) {
+    async fn test_handle_with_px(
+        store: crate::store::Store,
+        key: String,
+        value: String,
+        #[case] px: String,
+    ) {
         tokio::time::pause();
         let duration = 100;
         let args = vec![
             resp::RespType::SimpleString(key.clone()),
             resp::RespType::SimpleString(value.clone()),
-            resp::RespType::SimpleString("PX".into()),
+            resp::RespType::SimpleString(px),
             resp::RespType::SimpleString(duration.to_string()), // 100 milliseconds
         ];
         let response = handle(args, &store).await;
@@ -128,6 +126,125 @@ mod tests {
         assert_eq!(
             entry.deletion_time.expect("Checked it is some."),
             tokio::time::Instant::now()
+        );
+    }
+
+    // --- Errors ---
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_missing_key(store: crate::store::Store) {
+        let args = vec![];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError("ERR Missing key for 'SET' command".into()),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_invalid_key(store: crate::store::Store) {
+        let args = vec![resp::RespType::Array(vec![])];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError("ERR Failed to extract key for 'SET' command".into()),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_missing_value(store: crate::store::Store, key: String) {
+        let args = vec![resp::RespType::BulkString(Some(key))];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError("ERR Missing value for 'SET' command".into()),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_invalid_value(store: crate::store::Store, key: String) {
+        let args = vec![
+            resp::RespType::BulkString(Some(key)),
+            resp::RespType::Array(vec![]),
+        ];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError("ERR Failed to extract value for 'SET' command".into()),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_invalid_option(store: crate::store::Store, key: String, value: String) {
+        let args = vec![
+            resp::RespType::BulkString(Some(key)),
+            resp::RespType::BulkString(Some(value)),
+            resp::RespType::BulkString(Some("invalid option".into())),
+        ];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError(
+                "ERR invalid option is not a valid option for 'SET' command".into()
+            ),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_invalid_option_type(
+        store: crate::store::Store,
+        key: String,
+        value: String,
+    ) {
+        let args = vec![
+            resp::RespType::BulkString(Some(key)),
+            resp::RespType::BulkString(Some(value)),
+            resp::RespType::Array(vec![]),
+        ];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError("ERR Failed to extract option for 'SET' command".into()),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_missing_px_value(store: crate::store::Store, key: String, value: String) {
+        let args = vec![
+            resp::RespType::BulkString(Some(key)),
+            resp::RespType::BulkString(Some(value)),
+            resp::RespType::BulkString(Some("px".into())),
+        ];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError(
+                "ERR Missing milliseconds for PX option for 'SET' command".into()
+            ),
+            response
+        );
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn test_handle_invalid_px_value(store: crate::store::Store, key: String, value: String) {
+        let args = vec![
+            resp::RespType::BulkString(Some(key)),
+            resp::RespType::BulkString(Some(value)),
+            resp::RespType::BulkString(Some("px".into())),
+            resp::RespType::BulkString(Some("abc".into())),
+        ];
+        let response = handle(args, &store).await;
+        assert_eq!(
+            resp::RespType::BulkError(
+                "ERR Failed to convert PX duration string to a number for 'SET' command".into()
+            ),
+            response
         );
     }
 }
