cee5b03a48e3a963aad878c88b6828a39970f997
Keith Dao <keith.dao.248@gmail.com>
2025-10-24 19:13:26 +1000

    feat: Add command trait.

--- src/commands.rs
+++ src/commands.rs
@@ -3,3 +3,16 @@ pub mod get;
 pub mod ping;
 pub mod rpush;
 pub mod set;
+
+/// The command trait.
+pub trait Command {
+    /// Gets the name of the comamnd.
+    fn name(&self) -> String;
+
+    /// Runs the command.
+    async fn handle(
+        &self,
+        args: Vec<crate::resp::RespType>,
+        store: &crate::store::SharedStore,
+    ) -> crate::resp::RespType;
+}
